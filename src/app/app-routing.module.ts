9adc1f7af23ec88ef6285a9a4aad06ce61e743f3
VladislavZubko <vladyslav.zubko@nure.ua>
2019-01-08 00:24:02 +0200

    add routs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,23 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {SigninComponent} from './auth/signin/signin.component';
 import {DashboardComponent} from './admin/dashboard/dashboard.component';
+import {RoomsComponent} from './admin/rooms/rooms.component';
+import {ListComponent} from './admin/requests/list/list.component';
+import {AddComponent} from './admin/requests/add/add.component';
+import {ClientsComponent} from './admin/clients/clients.component';
 
 const appRoutes: Routes = [{
   path: 'login', component: SigninComponent
 }, {
   path: 'dashboard', component: DashboardComponent
+}, {
+  path: 'rooms', component: RoomsComponent
+}, {
+  path: 'clients', component: ClientsComponent
+}, {
+  path: 'add-request', component: AddComponent
+}, {
+  path: 'request-list', component: ListComponent
 }];
 
 @NgModule({
