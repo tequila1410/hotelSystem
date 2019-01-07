af07f21c06812c0679236f3e8bb4337ca40116aa
Vadim <shashkov.vadik@gmail.com>
2019-01-07 21:49:28 +0200

    add all dashboard information

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -8,8 +8,34 @@ export class DashboardComponent implements OnInit {
   public countEmptyRooms: number = 0;
   public countCurrentClients: number = 0;
   public countAllRequests: number = 0;
+  public bookings: any[] = [];
+  public showAdditionalInfo: boolean = false;
+  public selectedBooking: any = {};
   
   constructor(private dashboardService: DashboardService) {
+  }
+  
+  /**
+   * Toggle for additional information section
+   * @param booking
+   */
+  showInfo(booking) {
+    this.showAdditionalInfo = true;
+    this.selectedBooking = booking;
+    
+    this.selectedBooking.countDays = DashboardService.getDaysCount(this.selectedBooking.dateCheck, this.selectedBooking.dateEviction);
+    
+    this.dashboardService.getRoomById(this.selectedBooking.number).subscribe((room: any) => {
+      this.selectedBooking = {
+        ...this.selectedBooking, room: room[0]
+      };
+    });
+    
+    this.dashboardService.getUserById(this.selectedBooking.idClient).subscribe((user: any) => {
+      this.selectedBooking = {
+        ...this.selectedBooking, user: user[0]
+      };
+    });
   }
   
   ngOnInit() {
@@ -17,6 +43,10 @@ export class DashboardComponent implements OnInit {
       this.countEmptyRooms = response[0][0].countEmpty || 0;
       this.countCurrentClients = response[1][0].count || 0;
       this.countAllRequests = response[2][0].count || 0;
+    });
+    
+    this.dashboardService.getBookings().subscribe((response: any[]) => {
+      this.bookings = response;
     });
   }
 }
--- src/app/admin/dashboard/dashboard.service.ts
+++ src/app/admin/dashboard/dashboard.service.ts
@@ -8,18 +8,62 @@ import {environment} from '../../../environments/environment';
 })
 export class DashboardService {
   
+  /**
+   * Current date
+   * @type {string}
+   */
+  currentDate: string;
+  
   constructor(private http: HttpClient) {
+    const today = new Date();
+    this.currentDate = today.getFullYear() + '-' + today.getMonth() + 1 + '-' + today.getDate();
   }
   
   /**
    * Getting dashboard information
    */
-  getDashboardInformation() {
-    const today = new Date();
+  getDashboardInformation(): Observable<any> {
     const emptyRoomsUrl = environment.apiUrl + '/room/get/emptyRoom';
-    const currentClients = environment.apiUrl + '/booking/get/countCurrentClients/' + today.getFullYear() + '-' + today.getMonth() + 1 + '-' + today.getDate();
+    const currentClients = environment.apiUrl + '/booking/get/countCurrentClients/' + this.currentDate;
     const allRequests = environment.apiUrl + '/booking/get/countAllRequests/';
     
     return forkJoin(this.http.get(emptyRoomsUrl), this.http.get(currentClients), this.http.get(allRequests));
+  }
+  
+  /**
+   * Get all available bookings
+   */
+  getBookings(): Observable<any> {
+    return this.http.get(`${environment.apiUrl}/booking/get/endsBookings/${this.currentDate}`);
+  }
+  
+  /**
+   * Get user by id
+   * @param id
+   */
+  getUserById(id) {
+    return this.http.get(`${environment.apiUrl}/client/findby/id/${id}`);
+  }
+  
+  /**
+   * Get room by number
+   * @param number
+   */
+  getRoomById(number) {
+    return this.http.get(`${environment.apiUrl}/room/findby/number/${number}`);
+  }
+  
+  /**
+   * Get count days
+   * @param start
+   * @param end
+   */
+  static getDaysCount(start, end) {
+    const oneDay = 24 * 60 * 60 * 1000;
+    const startSplit = start.split('.');
+    const endSplit = end.split('.');
+    const dateStart = new Date(startSplit[2], startSplit[1], startSplit[0]);
+    const dateEnd = new Date(endSplit[2], endSplit[1], endSplit[0]);
+    return Math.round(Math.abs((dateEnd.getTime() - dateStart.getTime()) / (oneDay)));
   }
 }
